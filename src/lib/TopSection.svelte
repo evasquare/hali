6d45849da7265f35ae1ff560b56ea7f4b77de0b9
cosmostellar <160427240+cosmostellar@users.noreply.github.com>
2024-07-23 19:10:39 +0900

    Use text input as a subtitle

--- src/lib/TopSection.svelte
+++ src/lib/TopSection.svelte
@@ -1,10 +1,43 @@
-<div class="no-user-select">
-    <h1 class="text-left">TODOs</h1>
-    <p class="text-left">So uh... what was I going to do again?</p>
+<script lang="ts">
+    let text = '';
+</script>
+
+<div class="top-section no-user-select">
+    <h1 class="title text-left">TODOs</h1>
+    <input
+        type="text"
+        placeholder="So uh... what was I going to do again?"
+        class="message-input"
+        value={text}
+    />
 </div>
 
 <style>
-    .text-left {
+    .top-section {
+        width: 100%;
+    }
+    .top-section > .text-left {
         text-align: left;
+    }
+    .top-section > .title {
+        margin-bottom: 15px;
+    }
+
+    .message-input {
+        all: unset;
+
+        width: 100%;
+
+        padding: 0 0;
+        margin-bottom: 20px;
+
+        color: #0f0f0f;
+        font-size: 100%;
+
+        font-weight: 500;
+
+        @media (prefers-color-scheme: dark) {
+            color: white;
+        }
     }
 </style>
