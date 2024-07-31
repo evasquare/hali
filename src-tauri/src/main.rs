276f1669fd605effa1c49131229e9016c9bf5f7a
cosmostellar <160427240+cosmostellar@users.noreply.github.com>
2024-08-01 03:11:45 +0900

    Implement parser in Rust

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,14 +3,81 @@
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
+use std::error::Error;
+
+use serde::{Deserialize, Serialize};
+
+mod testing;
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Todo {
+    finished: bool,
+    text: String,
+}
+
+fn validate_each_line(line: &str) -> Result<Todo, Box<dyn Error>> {
+    let error_message = r#"Invalid Syntax:
+There's a line that doesn't start with `- [] ` or `- [x] `."#;
+
+    if line.starts_with("- [] ") {
+        let splitted_line = line.split("- [] ").last();
+        match splitted_line {
+            Some(splitted_line) => Ok(Todo {
+                finished: false,
+                text: splitted_line.to_string(),
+            }),
+            None => Err("Invalid Syntax".into()),
+        }
+    } else if line.starts_with("- [x] ") {
+        let splitted_line = line.split("- [x] ").last();
+        match splitted_line {
+            Some(splitted_line) => Ok(Todo {
+                finished: true,
+                text: splitted_line.to_string(),
+            }),
+            None => Err(error_message.into()),
+        }
+    } else {
+        Err("Invalid Syntax".into())
+    }
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct ParseResult {
+    successful: bool,
+    error_message: Option<String>,
+    todos: Vec<Todo>,
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn parse_hali_format(input: &str) -> ParseResult {
+    let mut result_todos: Vec<Todo> = vec![];
+
+    for line in input.trim().split('\n') {
+        match validate_each_line(line) {
+            Ok(todo) => {
+                result_todos.push(todo);
+            }
+            Err(e) => {
+                return ParseResult {
+                    successful: false,
+                    error_message: Some(e.to_string()),
+                    todos: vec![],
+                };
+            }
+        }
+    }
+
+    ParseResult {
+        successful: true,
+        error_message: None,
+        todos: result_todos,
+    }
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![parse_hali_format])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src-tauri/src/testing.rs
@@ -0,0 +1,64 @@
+mod parse_hali_format {
+    use super::super::parse_hali_format;
+    use crate::{ParseResult, Todo};
+
+    #[test]
+    fn case_1() {
+        let input = r#"- [] Task 1
+- [x] Task 2
+- [] Task 3"#;
+        let expected_output = ParseResult {
+            successful: true,
+            error_message: None,
+            todos: vec![
+                Todo {
+                    finished: false,
+                    text: "Task 1".to_string(),
+                },
+                Todo {
+                    finished: true,
+                    text: "Task 2".to_string(),
+                },
+                Todo {
+                    finished: false,
+                    text: "Task 3".to_string(),
+                },
+            ],
+        };
+
+        let output = parse_hali_format(input);
+
+        assert_eq!(output, expected_output);
+    }
+
+    #[test]
+    fn case_2() {
+        let input = r#"
+- [] Task 1
+- [x] Task 2
+- [] Task 3
+"#;
+        let expected_output = ParseResult {
+            successful: true,
+            error_message: None,
+            todos: vec![
+                Todo {
+                    finished: false,
+                    text: "Task 1".to_string(),
+                },
+                Todo {
+                    finished: true,
+                    text: "Task 2".to_string(),
+                },
+                Todo {
+                    finished: false,
+                    text: "Task 3".to_string(),
+                },
+            ],
+        };
+
+        let output = parse_hali_format(input);
+
+        assert_eq!(output, expected_output);
+    }
+}
