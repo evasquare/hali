32de867e30a8d79d87f107bcd686930897e0b1ce
cosmostellar <160427240+cosmostellar@users.noreply.github.com>
2024-08-01 04:12:51 +0900

    Stop showing duplicate `Error:` indicators

--- src/App.svelte
+++ src/App.svelte
@@ -57,7 +57,7 @@
                     />
                 {/each}
             {:catch error}
-                <span>Error: {error}</span>
+                <span>{error}</span>
             {/await}
             <div bind:this={endOfTodos} />
         </div>
